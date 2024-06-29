5bd1907dad55e6b9fbb2188becf00f1c7a80a8d4
ABHAY R NAIR <143316797+abhi00rnair@users.noreply.github.com>
2024-06-29 15:24:23 +0530

    Time: 19 ms (74.32%), Space: 20.6 MB (27.11%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(s.begin(),s.end());
+        sort(g.begin(),g.end());
+        return func(g,s);
+    }
+    
+    int func(vector<int>&g,vector<int>&s){
+        int i=0;
+        int j=0;
+        int count=0;
+        while(i<g.size() && j<s.size()){
+            if(s[j]>=g[i]){
+                count++;
+                j++;
+                i++;
+            }else{
+                j++;
+            }
+        }
+        return count;
+    }
+};
