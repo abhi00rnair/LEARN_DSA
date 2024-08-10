f5459d8d47681bedadab512ce713c2e8a0172d2a
ABHAY R NAIR <143316797+abhi00rnair@users.noreply.github.com>
2024-08-10 23:47:19 +0530

    Time: 7 ms (81.19%), Space: 20 MB (8.52%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int>hash;
+        unordered_map<int,int>hashh;
         int n=nums.size();
         vector<int>ret;
         for(int i=0;i<n;i++){
-            hash[nums[i]]++;
+            hashh[nums[i]]++;
         }
         int key=n/3;
-        for(auto it:hash){
+        for(auto it:hashh){
             if(it.second>key){
                 ret.push_back(it.first);
             }
