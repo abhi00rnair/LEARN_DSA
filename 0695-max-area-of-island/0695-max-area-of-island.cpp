d9df0e01994f3a1f4d706a35051d8cacaafa2c6b
ABHAY R NAIR <143316797+abhi00rnair@users.noreply.github.com>
2024-09-03 01:12:06 +0530

    Time: 21 ms (20.01%), Space: 34 MB (8.78%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int ret=0;
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>visit(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(visit[i][j]==0 && grid[i][j]==1){
+                    ret=max(ret,func(grid, visit,i,j,m,n));
+                }
+            }
+        }
+        return ret; 
+    }
+    int func(vector<vector<int>>&grid, vector<vector<int>>&visit,int i, int j, int m , int n){
+        queue<pair<int, int>>que;
+        int temp=1;
+        que.push({i,j});
+        visit[i][j]=1;
+            while (!que.empty()) {
+            pair<int, int> p = que.front();
+            que.pop();
+            int row = p.first;
+            int col = p.second;
+            vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+            for (auto dir : directions) {
+                int newRow = row + dir.first;
+                int newCol = col + dir.second;
+                if (newRow >= 0 && newRow < grid.size() && newCol >= 0 && newCol < grid[0].size() &&
+                    !visit[newRow][newCol] && grid[newRow][newCol] == 1) {
+                    que.push({newRow, newCol});
+                    visit[newRow][newCol] = 1;
+                    temp++;
+                }
+            }
+        }
+        return temp;
+    }
+
+};
