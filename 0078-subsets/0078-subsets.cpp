accdc545550e249c4f79bc5330d604fe73cd727b
ABHAY R NAIR <143316797+abhi00rnair@users.noreply.github.com>
2025-05-14 22:02:40 +0530

    Time: 1 ms (47.84%), Space: 15.3 MB (24.02%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>ret;
+        vector<vector<int>>rett;
+        vector<int>temp;
         int n=nums.size();
-        vector<int>arr;
-        func(nums,ret,arr,n,0);
-        return ret;
+        func(nums,temp,rett,0,n);
+        return rett;
     }
-    
-    void func(vector<int>&nums,vector<vector<int>>&ret,vector<int>&arr,int n, int i){
-        if(i>=n){
-            ret.push_back(arr);
+
+    void func(vector<int>nums, vector<int>&temp, vector<vector<int>>&rett, int i, int n){
+        if(i>n-1){
+            rett.push_back(temp);
             return;
         }
-        arr.push_back(nums[i]);
-        func(nums,ret,arr,n,i+1);
-        arr.pop_back();
-        func(nums,ret,arr,n,i+1);
+        temp.push_back(nums[i]);
+        func(nums,temp,rett,i+1,n);
+        temp.pop_back();
+        func(nums,temp,rett,i+1,n);
     }
+
 };
