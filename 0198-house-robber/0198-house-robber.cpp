8f33e863ab902d28c5a5c718541dcc1a43171572
ABHAY R NAIR <143316797+abhi00rnair@users.noreply.github.com>
2025-05-31 20:50:18 +0530

    Time: 0 ms (100%), Space: 10.7 MB (45.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,21 +3,17 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n+1,-1);
-        return func(nums,n-1,dp);
-
+        return func(nums, n-1, dp);
+        
     }
-    int func(vector<int>&nums, int n,vector<int>&dp){
-        if(n==0){
-            return  nums[n];
-        }
+
+    int func(vector<int>&nums,int n, vector<int>&dp){
         if(n<0){
             return 0;
         }
         if(dp[n]!=-1){
             return dp[n];
         }
-        int pick=nums[n]+func(nums,n-2,dp);
-        int nopick=0+func(nums,n-1,dp);
-        return dp[n]=max(pick,nopick);
+        return dp[n]=max(func(nums,n-1,dp), func(nums,n-2,dp)+nums[n]);
     }
 };
