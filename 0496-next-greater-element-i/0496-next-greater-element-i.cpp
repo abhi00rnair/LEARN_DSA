a5704ebaddbe1f10a4e055986e78ebf1f69621a9
ABHAY R NAIR <143316797+abhi00rnair@users.noreply.github.com>
2025-07-01 19:44:31 +0530

    Time: 0 ms (100%), Space: 12.9 MB (14.94%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int>mp;
+        stack<int>st;
         int n=nums2.size();
-        map<int, int>mp;
-        for(int i=0;i<n;i++){
-            int flag=1;
-            for(int j=i+1;j<n;j++){
-                if(nums2[i]<nums2[j]){
-                    mp[nums2[i]]=nums2[j];
-                    flag=0;
-                    break;
-                }
-            }if(flag==1){
-            mp[nums2[i]]=-1;
+        mp[nums2[n-1]]=-1;
+        st.push(nums2[n-1]);
+        for(int i=nums2.size()-2;i>=0;i--){
+            while(!st.empty() && st.top()<nums2[i]){
+                st.pop();
             }
+            if(st.empty()){
+                mp[nums2[i]]=-1;
+            }else{
+                mp[nums2[i]]=st.top();
+            }
+            st.push(nums2[i]);
         }
         vector<int>rett;
         for(int i=0;i<nums1.size();i++){
-            for(auto it:mp){
-                if(it.first==nums1[i]){
-                    rett.push_back(it.second);
-                    break;
-                }
-            }
+            rett.push_back(mp[nums1[i]]);
         }
         return rett;
     }
