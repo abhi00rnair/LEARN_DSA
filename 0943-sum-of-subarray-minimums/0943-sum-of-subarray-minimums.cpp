b230eea72eca965d3bf89da352c5ffe91490115f
ABHAY R NAIR <143316797+abhi00rnair@users.noreply.github.com>
2025-07-03 20:24:48 +0530

    Time: 15 ms (70.7%), Space: 47.3 MB (41.73%) - LeetHub

---
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int n=arr.size();
+        vector<int>pse(n,-1);
+        stack<int>st;
+        int i=1;
+        pse[0]=-1;
+        st.push(0);
+        while(i<n){
+            while(!st.empty() && arr[st.top()]>arr[i] ){
+                st.pop();
+            }
+            if(st.empty()){
+                pse[i]=-1;
+            }else{
+                pse[i]=st.top();
+            }
+            st.push(i);
+            i++;
+        }
+        stack<int>st1;
+        vector<int>nse(n,-1);
+        i=n-2;
+        nse[n-1]=n;
+        st1.push(n-1);
+        while(i>=0){
+            while(!st1.empty() && arr[st1.top()]>=arr[i]){
+                st1.pop();
+            }
+            if(st1.empty()){
+                nse[i]=n;
+            }else{
+                nse[i]=st1.top();
+            }
+            st1.push(i);
+            i--;
+        }
+
+        int rett=0,mod=1e9+7;
+        for(int i=0;i<n;i++){
+            int pse1=pse[i];
+            int nse1=nse[i];
+            long long contri=(nse1-i)*(i-pse1);
+            long long temp=contri*arr[i];
+            rett=(rett+temp)%mod;
+        }
+        return rett;
+    }
+
+
+};
