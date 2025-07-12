49064699c6556fc5e7cf66b7b0721d256e9bedd9
ABHAY R NAIR <143316797+abhi00rnair@users.noreply.github.com>
2025-07-12 22:48:14 +0530

    Time: 0 ms (100%), Space: 17.2 MB (69.81%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,26 +2,13 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,-1));
-        return func(prices,0,n,1,dp);
-    }
-    int func(vector<int>&prices, int i, int n, int count, vector<vector<int>>&dp){
-        if(i>=n){
-            return 0;
-        }
-        if(dp[i][count]!=-1){
-            return dp[i][count];
-        }
         int profit=0;
-        if(count==1){
-            int buy=-prices[i]+func(prices,i+1,n,0,dp);
-            int go=func(prices,i+1,n,1,dp);
-            profit=max(buy,go);
-        }else{
-            int sell=prices[i]+func(prices,i+1,n,1,dp);
-            int hold=func(prices,i+1,n,0,dp);
-            profit=max(sell,hold);
+
+        for(int i=1;i<n;i++){
+            if(prices[i]>prices[i-1]){
+                profit=profit+(prices[i]-prices[i-1]);
+            }
         }
-        return dp[i][count]=profit;
+        return profit;
     }
 };
