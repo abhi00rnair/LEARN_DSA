316ac3bf2ea0745d346fc141f2d1ca6035905bc5
ABHAY R NAIR <143316797+abhi00rnair@users.noreply.github.com>
2025-07-13 22:15:07 +0530

    Time: 301 ms (22.09%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT (SELECT e1.salary AS SecondHighestSalary FROM Employee e1 WHERE 1=(SELECT COUNT(DISTINCT e2.salary)FROM Employee e2 WHERE e2.salary>e1.salary) LIMIT 1) AS SecondHighestSalary
+SELECT (SELECT DISTINCT salary FROM Employee  ORDER BY salary DESC LIMIT 1 OFFSET 1 ) AS SecondHighestSalary; 
