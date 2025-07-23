3398d39cd76c62fbe79fac62fb01fbcc46c2db23
ABHAY R NAIR <143316797+abhi00rnair@users.noreply.github.com>
2025-07-23 15:56:16 +0530

    Time: 0 ms (100%), Space: 19.7 MB (58.24%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        queue<int>que;
-        int ret=0;
-        int n=isConnected.size();
-        int visited[n];
-        memset(visited,0,sizeof(int)*n);
-        /*for(int j=0;j<n;j++){
-            visited[j]=0;
-        }*/
-        for(int i=0;i<n;i++){
-            if(!(visited[i])){
-                func(isConnected,visited,que,i,n);
-                ret++;
+        int nodes=isConnected[0].size();
+        vector<int>visited(nodes, -1);
+        queue<int>Qq;
+        int rett=0;
+
+        for(int i=0;i<nodes;i++){
+            if(visited[i]==-1){
+                visited[i]=1;
+                Qq.push(i);
+                rett++;
             }
-        }
-        return ret;
-    }
-    void func(vector<vector<int>>&nums,int visited[],queue<int>&que, int i, int n){
-        que.push(i);
-        visited[i]=1;
-        while(!que.empty()){
-            int node=que.front();
-            que.pop();
-            for(int j=0;j<n;j++){
-                if(!(visited[j]) && nums[node][j]==1){
-                    que.push(j);
-                    visited[j]=1;
+            while(!Qq.empty()){
+                int temp=Qq.front();
+                Qq.pop();
+                for(int j=0;j<nodes;j++){
+                    if(visited[j]==-1 && isConnected[temp][j]==1){
+                        visited[j]=1;
+                        Qq.push(j);
+                    }
                 }
             }
         }
+        return rett;
     }
 };
