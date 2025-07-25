b183686452fc6ed47b23e1ecfa5799c67c9eb86c
ABHAY R NAIR <143316797+abhi00rnair@users.noreply.github.com>
2025-07-25 22:10:44 +0530

    Time: 0 ms (100%), Space: 18 MB (78.66%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -26,8 +26,6 @@ public:
                 }
             }
         }
-        if(rett.size()==numCourses){
-            return rett;
-        }return {};
+        return rett.size()==numCourses ? rett : vector<int>();
     }
 };
