904f57fc8bf8c3aa5205b66485f36326e63ec7a5
ABHAY R NAIR <143316797+abhi00rnair@users.noreply.github.com>
2025-07-26 15:54:29 +0530

    Time: 21 ms (51.8%), Space: 25.3 MB (56.78%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n=grid.size(),m=grid[0].size();
-        if(grid[0][0]==1 || grid[n-1][m-1]==1){
+        if(grid[0][0]==1){
             return -1;
         }
+        int n=grid.size();
+        int m=grid[0].size();
         queue<pair<int,int>>Qq;
-        vector<pair<int,int>>helper={{0,-1},{0,1},{1,0},{-1,0},{-1,1},{-1,-1},{1,1},{1,-1}};
-        Qq.push({0,0});
-        grid[0][0]=1;
-        while(!Qq.empty()){
-            int i=Qq.front().first;
-            int j=Qq.front().second;
-            Qq.pop();
-            int dist=grid[i][j];
-            if(i==n-1 && j==m-1){
-                return dist;
-            }
-            for(auto it:helper){
-                int newi=i+it.first;
-                int newj=j+it.second;
-                if(newi>=0 && newj>=0 && newi<n && newj<m &&grid[newi][newj]==0){
-                    Qq.push({newi,newj});
-                    grid[newi][newj]=dist+1;
-                }
-            }
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
+        dist[0][0]=1;
 
+        Qq.push({0,0});
+        vector<int>tp{-1,0,0,1,-1,-1,1,1};
+        vector<int>tr{0,1,-1,0,1,-1,1,-1};
+
+        while(!Qq.empty()){
+            int row=Qq.front().first;
+            int colo=Qq.front().second;
+            int distt=dist[row][colo];
+            if(row==n-1 && colo==m-1){
+                return distt;
+            }
+            Qq.pop();
+            for(int x=0;x<8;x++){
+                    int newrow=row+tp[x];
+                    int newcolo=colo+tr[x];
+                    if(newrow>=0 && newcolo>=0 && newrow<n && newcolo<m && grid[newrow][newcolo]==0 && dist[newrow][newcolo]==INT_MAX){
+                        dist[newrow][newcolo]=distt+1;
+                        Qq.push({newrow,newcolo});
+                    }
+            }
         }
         return -1;
+
     }
 };
