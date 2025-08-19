9bb907052b0953adf58abf9497ce5dea2934541d
ABHAY R NAIR <143316797+abhi00rnair@users.noreply.github.com>
2025-08-19 15:33:39 +0530

    Time: 0 ms (100%), Space: 52.4 MB (20.26%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n, -1);
-        return func(0,nums, n, dp);
-        
-    }
-    bool func(int i, vector<int>&nums, int n, vector<int>&dp){
-        if(i==n-1){
+        int maxxi=0;
+
+        for(int i=0;i<nums.size();i++){
+            if(i>maxxi){
+                return false;
+            }
+            maxxi=max(maxxi,nums[i]+i);
+        }
+        if(maxxi>=nums.size()-1){
             return true;
-        }
-        if(i>=n){
-            return false;
-        }
-                    if(dp[i]!=-1){
-                return dp[i];
-            }
-        for(int j=1;j<=nums[i]; j++){
-            if(i+j<n){
-                dp[i]=func(i+j, nums, n, dp);
-                if(dp[i]==1){
-                    return true;
-                }
-            }
-        }
-    return dp[i]=false;
+        }return false;
     }
 };
