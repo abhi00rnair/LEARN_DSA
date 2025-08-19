19e00a813753b562520ae90943c4a041e728c3ee
ABHAY R NAIR <143316797+abhi00rnair@users.noreply.github.com>
2025-08-19 16:08:13 +0530

    Time: 0 ms (100%), Space: 20.5 MB (62.96%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        vector<int>dp(nums.size(), -1);
-        return func(nums, 0, nums.size(), dp);
-    }
+        int l=0;
+        int r=0;
+        int jumps=0;
+        int n=nums.size();
 
-    int func(vector<int>&nums, int i, int n, vector<int>&dp){
-        if(i>=n-1){
-            return 0;
-        }if(nums[i]==0 || i>=n){
-            return 1e9;
+        while(n-1>r){
+            int far=0;
+            for(int j=l;j<=r;j++){
+                far=max(far, nums[j]+j);
+            }
+            l=r+1;
+            r=far;
+            jumps++;
         }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int rett=1e9;
-        for(int j=1;j<=nums[i];j++){
-            int tp=1+func(nums, i+j,n, dp);
-            rett=min(rett, tp);
-        }
-        return dp[i]=rett;
+        return jumps;
     }
 };
