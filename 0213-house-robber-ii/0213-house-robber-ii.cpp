830af394ca1f4ef9f5c1514185fc015a30393171
ABHAY R NAIR <143316797+abhi00rnair@users.noreply.github.com>
2025-11-30 16:50:06 +0530

    Time: 0 ms (100%), Space: 11 MB (19.18%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1){
+        if(nums.size()==1){
             return nums[0];
-        }        
-        vector<int>dp(n, -1);
-        vector<int>dp2(n,-1);
-        int fun1=func(nums,n-2,0,dp);
-        int fun2=func(nums,n-1,1,dp2);
-        return max(fun1, fun2);
-        
+        }
+        vector<int>arr1(nums.begin(), nums.end()-1);
+        vector<int>arr2(nums.begin()+1, nums.end());
+        vector<int>dp1(nums.size(), -1);
+        vector<int>dp2(nums.size(), -1);
+        return max(func(arr1,0, dp1), func(arr2,0, dp2));
     }
-    int func(vector<int>&nums, int n ,int end, vector<int>&dp){
-        if(n<end){
+
+    int func(vector<int>&nums, int i, vector<int>&dp){
+        if(i>=nums.size()){
             return 0;
         }
-        if(dp[n]!=-1){
-            return dp[n];
+        if(dp[i]!=-1){
+            return dp[i];
         }
-        return dp[n]=max(func(nums,n-1,end,dp), func(nums,n-2,end,dp)+nums[n]);
+        int tp=nums[i]+func(nums,i+2,dp);
+        int tr=func(nums, i+1,dp);
+        return dp[i]=max(tp,tr);
     }
 };
