76c8259bff13a1fa727c8bc75b00b18e0a47c699
ABHAY R NAIR <143316797+abhi00rnair@users.noreply.github.com>
2026-03-04 15:12:26 +0530

    Time: 4 ms (26.16%), Space: 9.7 MB (12.79%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        return func(s,p,0,0);
+        int n=s.size(),m=p.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return func(s,p,0,0,dp);
     }
 
-    bool func(string&s, string&p , int i, int j){
+    bool func(string&s, string&p , int i, int j, vector<vector<int>>&dp){
         if(j>=p.size()){
-            return i==s.size();
+            return dp[i][j]= i==s.size();
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
         bool match=(i<s.size() && (s[i]==p[j] || p[j]=='.'));
         if(j+1<p.size() && p[j+1]=='*'){
-            return func(s,p,i,j+2) || (match &&func(s,p,i+1,j));
+            return dp[i][j]=func(s,p,i,j+2,dp) || (match &&func(s,p,i+1,j,dp));
         }
         if(match){
-            return func(s,p,i+1,j+1);
+            return dp[i][j]=func(s,p,i+1,j+1,dp);
         }
-        return false;
+        return dp[i][j]=false;
     }
 };
