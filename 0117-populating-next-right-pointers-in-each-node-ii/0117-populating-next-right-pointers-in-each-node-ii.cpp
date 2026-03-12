9700930bbca743e08eff53c7ece5ec42c32bdf5d
ABHAY R NAIR <143316797+abhi00rnair@users.noreply.github.com>
2026-03-13 00:42:16 +0530

    Time: 12 ms (31.16%), Space: 18.8 MB (52.55%) - LeetHub

---
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -0,0 +1,53 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root==NULL){
+            return NULL;
+        }
+        queue<Node*>qq;
+        qq.push(root);
+        while(!qq.empty()){
+            int n=qq.size();
+            for(int i=0;i<n-1;i++){
+                Node*tp=qq.front();
+                qq.pop();
+                tp->next=qq.front();
+                if(tp->left!=NULL){
+                    qq.push(tp->left);
+                }
+                if(tp->right!=NULL){
+                    qq.push(tp->right);
+                }
+            }
+            if(qq.front()->left!=NULL){
+                qq.push(qq.front()->left);
+            }
+            if(qq.front()->right!=NULL){
+                qq.push(qq.front()->right);
+            }
+            qq.front()->next=NULL;
+            qq.pop();
+
+        }
+        return root;
+    }
+
+};
