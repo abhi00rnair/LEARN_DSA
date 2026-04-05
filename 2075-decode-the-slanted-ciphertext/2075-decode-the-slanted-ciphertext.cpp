2e639440ea8d596f199e9386b62ca356f6796963
ABHAY R NAIR <143316797+abhi00rnair@users.noreply.github.com>
2026-04-05 18:37:55 +0530

    Time: 41 ms (41.03%), Space: 49.1 MB (22.22%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     string decodeCiphertext(string encodedText, int rows) {
-        if(rows==0){
-            return "";
-        }
         int ss=encodedText.size();
         int m=ss/rows;
+        vector<vector<char>>matrix(rows,vector<char>(m));
+        int k=0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<m;j++){
+                matrix[i][j]=encodedText[k];
+                k++;
+            }
+        }
         string rett="";
         int x=0;
         while(x<m){
             int i=0,j=x;
             while(i<rows && j<m){
-                rett.push_back(encodedText[i*m+j]);
+                rett.push_back(matrix[i][j]);
                 i++;
                 j++;
             }
